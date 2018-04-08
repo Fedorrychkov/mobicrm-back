6749dcf087b2879edfe4f0f9d00d537206758127
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-08 23:01:20 +0300

    Add new route

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-bodyparser')();
+const { OrdersController } = require('./controllers/orders');
 
 const router = new Router();
 router.get('/', ctx => {
-    ctx.body = 'hop hey'
+    ctx.body = 'Welcome to MobiCRM service'
 });
+router.get('/company/:id/orders', OrdersController);
 router.post('/', bodyParser, ctx => {
     ctx.body = { data: ctx.request.body }
 });
