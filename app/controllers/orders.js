51cb49bbd8597d96da60f159f16a1515deab6210
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-08 22:59:58 +0300

    Add orders controller, simple get endpoint

---
+++ app/controllers/orders.js
@@ -0,0 +1,35 @@
+const { Orders } = require('../models/orders');
+
+const OrdersController = async ctx => {
+    const collection = await Orders.findAll({where: {company_id: ctx.params.id}});
+    let response = {}
+    try {
+        if (collection.length > 0) {
+            response = { 
+                body: collection, 
+                length: collection.length, 
+                status: 200, 
+                status_text: 'OK'
+            }
+        } else {
+            response = { 
+                body: collection, 
+                length: collection.length, 
+                status: 204,
+                status_text: 'No Content'
+            }
+        }
+    } catch (ex) {
+        response = { 
+            body: {},
+            length: 0, 
+            status: 500, 
+            status_text: 'Internal Server Error'
+        }
+    }
+    ctx.body = response;
+}
+
+module.exports = {
+    OrdersController,
+}
