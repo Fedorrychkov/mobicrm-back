6551d07b9d55ec52de1233295ad8f8434eedadea
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-15 20:40:52 +0300

    Add new rule in DirectorSignupController

--- app/controllers/directors/SignupController.js
+++ app/controllers/directors/SignupController.js
@@ -5,34 +5,41 @@ const DirectorSignupController = async ctx => {
     let response = {}
     const request = ctx.request.body;
     try {
-        const hasUserByLogin = await Directors.findAll({where: {login: request.login}});
-        const hasUserByEmail = await Directors.findAll({where: {email: request.email}});
-        if (hasUserByLogin.length > 0 || hasUserByEmail.length > 0) {
+        if (request.password == undefined || request.login == undefined || request.email == undefined) {
             response = {
                 body: {},
                 status: 400,
-                status_text: 'Логин или email заняты'
+                status_text: 'Bad Request'
             }
         } else {
-            let cryptoHash = {
-                salt: '',
-                passHash: ''
-            };
-            cryptoHash= await createPassAndSaltHas(request.password);
-            // ctx.session.pass = passHash.toString('base64');
-            const res = await Directors.create({
-                login: request.login,
-                password: cryptoHash.passHash,
-                salt: cryptoHash.salt,
-                first_name: request.first_name,
-                last_name: request.last_name,
-                email: request.email
-            });
-            if (res) {
+            const hasUserByLogin = await Directors.findAll({where: {login: request.login}});
+            const hasUserByEmail = await Directors.findAll({where: {email: request.email}});
+            if (hasUserByLogin.length > 0 || hasUserByEmail.length > 0) {
                 response = {
-                    body: {token: ''},
-                    status: 201,
-                    status_text: 'Created'
+                    body: {},
+                    status: 400,
+                    status_text: 'Логин или email заняты'
+                }
+            } else {
+                let cryptoHash = {
+                    salt: '',
+                    passHash: ''
+                };
+                cryptoHash= await createPassAndSaltHas(request.password);
+                const res = await Directors.create({
+                    login: request.login,
+                    password: cryptoHash.passHash,
+                    salt: cryptoHash.salt,
+                    first_name: request.first_name,
+                    last_name: request.last_name,
+                    email: request.email
+                });
+                if (res) {
+                    response = {
+                        body: {token: ''},
+                        status: 201,
+                        status_text: 'Created'
+                    }
                 }
             }
         }
