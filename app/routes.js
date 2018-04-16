0ad872222cc21cad5668e85d854edb0d9b08f556
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-17 01:55:03 +0300

    Add some comment-endpoints

--- app/routes.js
+++ app/routes.js
@@ -9,14 +9,43 @@ const { CreateCompanyController } = require('./controllers/companies/');
 
 const router = new Router();
 
-/** Director Endpoints */
+/** 
+ * Director Endpoints
+ */
 router.post('/director', bodyParser, DirectorSignupController);
+// router.put('/director', bodyParser, {}); // change info about director
 router.post('/director/login', bodyParser, DirectorLoginController);
 
-/** Company Endpoints */
+/** 
+ * Company Endpoints
+ */
 router.post('/company', bodyParser, CreateCompanyController);
+// router.put('/company', bodyParser, {}); // Change info about company
+// router.get('/company/:id', {}); // Get one company
+// router.get('/director/:id/company', {}); // Get List of company by director id if role == director (1)
+
+/** 
+ * Orders Endpoints
+ */
 router.post('/company/orders', bodyParser, CreateNewOrderController);
-router.get('/company/:id/orders', GetOrdersByCompanyIdController); // Для сортировки добавить больше ссылок, 
+// router.put('/company/:id/orders/:orderId', bodyParser, {}); // change info about order
+router.get('/company/:id/orders', GetOrdersByCompanyIdController); // Для сортировки добавить больше ссылок,
 router.get('/company/:id/orders/:orderId', GetOrderByCompanyIdController);
+
+/**
+ * Customers Endpoints
+ */
+// router.post('/company/:id/customers', bodyParser, {}); // Create new customer
+// router.get('/company/:id/customers', {}); // Get list of customer in company
+// router.get('/company/:id/customers/:customerId', {}); // Get one of customer in company
+// router.put('/company/:id/customers/:customerId', {}); // Change info about customer in company
+
+/**
+ * Employees Endpoints
+ */
+// router.post('/company/:id/employees', bodyParser, {}); // Create new Employee
+// router.get('/company/:id/employees', {}); // Employees list of company
+// router.get('/company/:id/employees/:employeeId', {}); // One employee of company
+// router.post('/employees/login', bodyParser, {}); // login for employees ?
 
 module.exports = router;
