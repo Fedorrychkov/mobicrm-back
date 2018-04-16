8b4020afde8aa51b981fc5306c1ab9bc9a2a118b
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-16 22:07:16 +0300

    Add CreateNewOrderController, add some rules

--- app/controllers/orders/CreateNewOrderController.js
+++ app/controllers/orders/CreateNewOrderController.js
@@ -1,20 +1,36 @@
 const { Orders } = require('../../models/orders'),
+      { Customers } = require('../../models/customers'),
       passport = require('koa-passport'),
       jwt = require('jsonwebtoken'),
       jwtConfig = require('../../../config/jwt.json'),
       { INTERNAL_ERROR } = require('../../constants/error'),
       { CREATED } = require('../../constants/success');
 
+/**
+ * Create a new Order.
+ */
 const CreateNewOrderController = async (ctx, next) => {
     await passport.authenticate('jwt', async (err, user) => {
         let response = {}
         try {
             if (user) {
                 const req = ctx.request.body;
+                req.customer.company_id = req.company_id; // Add company_id in customer model
+                req.who_created = user.id;
+                if (req.customer) { // if we have customer property, we have try find customer by phone
+                    const checkPhone = await Customers.findOne({where: {phone: req.customer.phone}});
+                    if(!checkPhone) { // if we can't find this, create new customer
+                        const customer = await Customers.create(req.customer);
+                        req.customer_id = customer.id;
+                    } else {
+                        req.customer_id = checkPhone.id;
+                    }
+                }
                 const res = await Orders.create(req);
                 if (res) {
+                    const resCustomer = await Customers.findById(res.customer_id); // add more information in response about customer
                     response = {
-                        body: res,
+                        body: {order: res, customer: resCustomer},
                         status: CREATED.status,
                         status_text: CREATED.status_text
                     }
