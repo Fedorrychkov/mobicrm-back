063b4e9a97e111d7b745038f4cf33f5415505b34
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-16 19:52:46 +0300

    Add more information about customer in server response

--- app/controllers/orders/GetOrderByCompanyIdController.js
+++ app/controllers/orders/GetOrderByCompanyIdController.js
@@ -1,4 +1,5 @@
 const { Orders } = require('../../models/orders'),
+      { Customers } = require('../../models/customers'),
       passport = require('koa-passport'),
       { INTERNAL_ERROR } = require('../../constants/error'),
       { CREATED, OK } = require('../../constants/success');
@@ -8,10 +9,15 @@ const GetOrderByCompanyIdController = async (ctx, next) => {
         let response = {}
         try {
             if (user) {
-                const collection = await Orders.findOne({where: {company_id: ctx.params.id, id: ctx.params.orderId}});
-                if (collection) {
-                    response = { 
-                        body: collection,
+                const res = await Orders.findOne({where: {company_id: ctx.params.id, id: ctx.params.orderId}});
+                const customer = await Customers.findOne({where: {id: res.customer_id}});
+                if (res && customer) {
+                    const newRes = {
+                        order: res,
+                        customer
+                    }
+                    response = {
+                        body: newRes,
                         status: OK.status, 
                         status_text: OK.status_text
                     }
