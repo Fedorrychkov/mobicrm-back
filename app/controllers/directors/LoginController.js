b606547bc69b5d72337c4bd79f8652c3d57163a0
fedorrychkov <fedorrychkov@yandex.ru>
2018-04-25 23:04:51 +0300

    Add try catch block for director login controller

--- app/controllers/directors/LoginController.js
+++ app/controllers/directors/LoginController.js
@@ -1,38 +1,49 @@
 const passport = require('koa-passport'),
       jwt = require('jsonwebtoken'),
       jwtConfig = require('../../../config/jwt.json'),
-      { INTERNAL_ERROR } = require('../../constants/error'),
+      { INTERNAL_ERROR, BAD_REQUEST } = require('../../constants/error'),
       { OK } = require('../../constants/success');
 
 
 const DirectorLoginController = async (ctx, next) => {
-  await passport.authenticate('local', (err, user) => {
-    response = {};
-    if (user == false) {
-        response = { 
-            body: err, 
-            status: INTERNAL_ERROR.status,
-            status_text: INTERNAL_ERROR.status_text
-        }
-        ctx.body = response;
-    } else {
-        const payload = {
-            id: user.id,
-            displayName: user.login,
-            email: user.email
-        };
-        const token = jwt.sign(payload, jwtConfig.mysecretkey);
+    try {
+        await passport.authenticate('local', (err, user) => {
+            response = {};
+            if (user == false) {
+                response = { 
+                    body: err, 
+                    status: INTERNAL_ERROR.status,
+                    status_text: INTERNAL_ERROR.status_text
+                }
+                ctx.body = response;
+            } else {
+                const payload = {
+                    id: user.id,
+                    displayName: user.login,
+                    email: user.email
+                };
+                const token = jwt.sign(payload, jwtConfig.mysecretkey);
+                ctx.body = {
+                    body: {
+                        id: user.id,
+                        role: user.role,
+                        user: user.login,
+                        email: user.email 
+                    }, 
+                    token: token,
+                    status: OK.status,
+                    status_text: OK.status_text
+                };
+            }
+        })(ctx, next);
+    } catch (ex) {
         ctx.body = {
-            body: {
-                user: user.login,
-                email: user.email 
-            }, 
-            token: token,
-            status: OK.status,
-            status_text: OK.status_text
+            body: {textError: 'Логин или пароль не верны!'}, 
+            token: '',
+            status: BAD_REQUEST.status,
+            status_text: BAD_REQUEST.status_text
         };
     }
-  })(ctx, next);
 }
 
 module.exports = {
