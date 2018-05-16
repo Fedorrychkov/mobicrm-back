adecb35bb1dd19083568fc637ce7597956a863d3
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-16 21:01:20 +0300

    add all order statuses [in progress]

---
+++ app/constants/orderStatuses.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports.ORDER_DONE = 'DONE';
+
+module.exports.ORDER_NEW = 'NEW';
+
+module.exports.ORDER_THINK = 'CUSTOMER_THINK';
+
+module.exports.ORDER_LATER = 'CALL_LATER';
