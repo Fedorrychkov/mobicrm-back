4d4e9cb07f46b71eb37f56f2cd5e6716a92853de
fedorrychkov <fedorrychkov@yandex.ru>
2018-05-19 00:35:35 +0300

    blabla

---
+++ app/constants/roles.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports.directorRoleId = 1;
+module.exports.seniorManagerRoleId = 2;
+
+module.exports.DirectorRole = {
+    id: 1,
+    name: 'Директор',
+    type: 'DIRECTOR'
+};
+
+module.exports.SeniorManagerRole = {
+    id: 2,
+    name: 'Ст. менеджер',
+    type: 'SENIOR_MANAGER'
+};
+module.exports.ManagerRole = {
+    id: 3,
+    name: 'Менеджер',
+    type: 'MANAGER'
+};
+
+module.exports.WorkerRole = {
+    id: 4,
+    name: 'Монтажник',
+    type: 'WORKER'
+};
